66c10325dbe45b7b4bcbbdf69f08f2dd31103e84
moran.liu <moran.liu@quectel.com>
2023-04-28 16:03:28 +0800

    feat: 页面样式调整

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,27 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="app-nav">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+  <div class="app-container">
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.app-nav {
+  width: 120px;
+  height: 100%;
+  box-shadow: 0 0 5px 2px #CCCCCC;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.app-container {
+  overflow: hidden;
+  height: 100%;
+  flex: auto;
 }
 
 nav {
@@ -47,39 +43,13 @@ nav a.router-link-exact-active:hover {
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
+  /*display: inline-block;*/
+  display: block;
+  padding: 1rem 0;
   border-left: 1px solid var(--color-border);
 }
 
 nav a:first-of-type {
   border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="page-about">
+    <h1>About page</h1>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.page-about {
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  padding: 24px;
 }
 </style>
